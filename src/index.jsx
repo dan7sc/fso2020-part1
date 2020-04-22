faa8f75be7dba67375181e009e92dc87f23db375
dan7sc <su7cheng@gmail.com>
2020-04-21 21:34:40 -0300

    Add solution to exercise 1.12*: anecdotes step1

--- src/index.jsx
+++ src/index.jsx
@@ -1,72 +1,41 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistics = (props) => {
-    const {good, neutral, bad} = props
-    const all = good + neutral + bad
-    const average = ((1 * good) + (0 * neutral) + (-1 * bad)) / all
-    const positive = 100 * (good / all) + ' %'
-
-    if (good || neutral || bad) {
-        return (
-            <table>
-              <tbody>
-                <Statistic text='good' value={good} />
-                <Statistic text='neutral' value={neutral} />
-                <Statistic text='bad' value={bad} />
-                <Statistic text='all' value={all} />
-                <Statistic text='average' value={average} />
-                <Statistic text='positive' value={positive} />
-              </tbody>
-            </table>
-        )
-    }
-    return <div>No feedback given</div>
-}
-
-const Statistic = (props) => {
-    const {text, value} = props
-    return (
-        <tr>
-          <td>{text}</td>
-          <td>{value}</td>
-        </tr>
-    )
-}
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 const Button = (props) => {
-    const {text, handleClick} = props
+    const {handleClick, text} = props
 
     return (
         <button onClick={handleClick}>{text}</button>
     )
 }
 
-const App = () => {
-    // save clicks of each button to own state
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+const App = (props) => {
+    const [selected, setSelected] = useState(0)
+    const {anecdotes} = props
 
-    const handleGoodClick = () => setGood(good + 1)
-    const handleNeutralClick = () => setNeutral(neutral + 1)
-    const handleBadClick = () => setBad(bad + 1)
+    const handleClick = () => {
+        const newSelected = Math.round(Math.random() * 5)
+        setSelected(newSelected)
+    }
 
     return (
         <div>
-          <h1>give feedback</h1>
-          <div>
-            <Button handleClick={handleGoodClick} text='good' />
-            <Button handleClick={handleNeutralClick} text='neutral' />
-            <Button handleClick={handleBadClick} text='bad' />
-          </div>
-          <h1>statistics</h1>
-          <Statistics good={good} neutral={neutral} bad={bad} />
+          <div>{anecdotes[selected]}</div>
+          <Button handleClick={handleClick} text='next anecdote' />
         </div>
     )
 }
 
 ReactDOM.render(
-    <App />,
+    <App anecdotes={anecdotes} />,
     document.getElementById('root')
 )
